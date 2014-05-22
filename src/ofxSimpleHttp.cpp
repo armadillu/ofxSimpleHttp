cf6381d18223adbb843604e27e65fcaaab2d7c48
Oriol Ferrer Mesià <oriol@uri.cat>
2014-05-21 23:39:30 -0400

    minor tweak

--- src/ofxSimpleHttp.cpp
+++ src/ofxSimpleHttp.cpp
@@ -221,7 +221,7 @@ string ofxSimpleHttp::drawableString(){
 		"//\n"
 		"//   " + r->url + "\n" +
 		"//   Progress: " + ofToString(100.0f * r->downloadProgress, 2) + "%\n" +
-		"//   Server Reported Size: " + ofToString(r->serverReportedSize / (1024  * 1024), 2) + "Mb\n" +
+		"//   Server Reported Size: " + ofToString(r->serverReportedSize / float(1024  * 1024), 1) + "Mb\n" +
 		"//   Download Speed: " + ofToString(speed, 2) + speedUnit + "\n" +
 		"//   Time Taken so far: " + ofToString(timeSoFar, 1) + soFarTimeUnit + "\n" +
 		"//   Estimated Remaining Time: " + ofToString(timeRemaining, 1) + remtimeUnit + "\n" +
