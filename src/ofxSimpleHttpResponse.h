6c11cb7d73099e4c9664fdef16d4e6dc41d8080f
Oriol Ferrer Mesia <oriol@uri.cat>
2017-12-09 17:35:19 -0500

    add missing file

---
+++ src/ofxSimpleHttpResponse.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Poco/Timestamp.h"
+
+class ofxSimpleHttp;
+
+struct ofxSimpleHttpResponse{
+
+	ofxSimpleHttp * 			who;				//who are you getting the event from?
+	bool						ok;
+	bool						notifyOnSuccess;	// user wants to be notified when download is ready
+	bool						downloadCanceled;	// flag to cancel download
+	bool						downloadToDisk;		// user wants bytes on disk; otherwise just return data as string in "responseBody"
+	bool						emptyWholeQueue;	// flag
+	bool						checksumOK;			// SHA1 checksum matches
+	bool						fileWasHere;		// didnt even have to download the file!
+	std::string					expectedChecksum;	// sha1
+	int							status; 			// return code for the response ie: 200 = OK
+	long int					serverReportedSize;
+	std::string					reasonForStatus;	// text explaining the status
+	std::string					responseBody;		// the actual response << DATA IS HERE!
+	std::string					contentType;		// the mime type of the response
+	Poco::Timestamp				timestamp;			// time of the response
+	float						downloadProgress;	// [0..1]
+	std::streamsize				downloadedSoFar;
+	float						downloadSpeed;		// bytes/sec, only >0 when downloading, immediate
+	bool						chunkTested;
+	float						avgDownloadSpeed;	// bytes/sec, read after download happened
+	long int					downloadedBytes;
+	std::string					url;
+	std::string					fileName;			// file + extension, no path
+	std::string					extension;			// file extension (no dot)
+	std::string					absolutePath;		// where file was saved
+	float						timeTakenToDownload;// seconds
+	float						timeDowloadStarted; //from ofGetElapsedTimef()
+
+	std::string					customField;		//to be supplied when starting a download
+	//so that you can identify the callback when you get it
+	ofxSimpleHttpResponse();
+	void print();
+	std::string toString();
+};
+
